251fd733e682baab91ced94073a87ac6600893d0
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-06-04 21:28:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace ChatServer
+{
+    public class Program
+    {
+        private static Socket serverSocket;
+
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
